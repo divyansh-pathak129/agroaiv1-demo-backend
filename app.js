90455cdc8269326106160ff189738a71c79f4f5b
divyansh-pathak129 <divyansh.pathak129@gmail.com>
2024-08-30 11:21:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const server = http.createServer(app)
 const io = new Server(server, {
     cors:{
         // origin: process.env.CORS_URL,
-        origin: 'http://localhost:3000',
+        origin: process.env.URL,
         methods: ["GET", "POST"],
         credentials: true
     }
